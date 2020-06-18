7b93ab46bfb1aed899a75d1aa5e525c708d20afd
mateusanjost <mateusanjosmgb@gmail.com>
2020-06-18 20:29:57 -0300

    "..." function complet

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,14 @@
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
+  
+  // tooltip
+  $('[data-toggle="tooltip"]').tooltip()
+  
+  // tres function
+  $('.tres').click(function(){
+    $('.configs').toggle()
+  });
+  $('li:contains("Cancelar")').click(function(){
+    $('.configs').hide()
+  });
+  
   })
