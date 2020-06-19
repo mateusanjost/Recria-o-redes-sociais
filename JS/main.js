cdd352d132ea92ebf53eeea805040d4c86abae88
mateusanjost <mateusanjosmgb@gmail.com>
2020-06-19 11:26:08 -0300

    Alternar entre perfil e índice

--- JS/main.js
+++ JS/main.js
@@ -1,33 +1,45 @@
 $(function () {
 
   // tooltip
-  $('[data-toggle="tooltip"]').tooltip()
-
+  $('[data-toggle="tooltip"]').tooltip();
   // tres function
   $('.tres').click(function () {
-    $('.configs').toggle()
+    $('.configs').toggle();
     var denunciar = $(this).parents(".card");
+    var srcimg = "img[src='" + $(this).parents('.card').find('.card-header img').attr('src') + "']";
     $('li:contains("Cancelar")').click(function () {
-      $('.configs').hide()
+      $('.configs').hide();
     });
     $('li:contains("Denunciar")').click(function () {
       denunciar.remove();
-      $('.configs').hide()
+      $('.configs').hide();
       if ($('.card').length == 1) {
         $('.posts').html("<div class='msgnofeed'>Não há conteúdo no feed.</div>")
       }
     });
+    $('li:contains("Deixar de seguir")').click(function () {
+      denunciar.remove();
+      $(srcimg).parents('.card').remove();
+      $('.configs').hide();
+
+    });
   });
+
+  // perfil
+  $('.seta').click(function(){
+    $('i.fa.fa-camera, .input-group.flex-nowrap, .card, .storys').show();
+    $('.seta').hide();
 });
-$(function() {
-// auto complet
-  var availableTags = [
-    "Damon Salvatore",
-    "Bonnie Benedit",
-    "Elena Gilbert",
-    "Stefan Salvatore",
-  ];
-  $( "#tags" ).autocomplete({
-    source: availableTags
+  $('.card-header img, .storys img').click(function(){
+    var srcimg = $(this).attr('src');
+    $('i.fa.fa-camera, .input-group.flex-nowrap, .storys').hide();
+    $('.seta').show();
+    $('.posts .card').each(function(){
+      var corpo = $(this);
+      if($(this).find('.card-header img').attr("src") != srcimg) {
+        corpo.hide();
+      }
+    });
   });
-})
+  
+});
