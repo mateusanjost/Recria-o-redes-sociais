f8f867b7f7539a93d0b42273414a9a333d58fbda
mateusanjost <mateusanjosmgb@gmail.com>
2020-06-19 01:10:40 -0300

    autocomplet +  funcao search

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,33 @@
 $(function () {
-  
+
   // tooltip
   $('[data-toggle="tooltip"]').tooltip()
-  
+
   // tres function
-  $('.tres').click(function(){
+  $('.tres').click(function () {
     $('.configs').toggle()
+    var denunciar = $(this).parents(".card");
+    $('li:contains("Cancelar")').click(function () {
+      $('.configs').hide()
+    });
+    $('li:contains("Denunciar")').click(function () {
+      denunciar.remove();
+      $('.configs').hide()
+      if ($('.card').length == 1) {
+        $('.posts').html("<div class='msgnofeed'>Não há conteúdo no feed.</div>")
+      }
+    });
   });
-  $('li:contains("Cancelar")').click(function(){
-    $('.configs').hide()
+});
+$(function() {
+// auto complet
+  var availableTags = [
+    "Damon Salvatore",
+    "Bonnie Benedit",
+    "Elena Gilbert",
+    "Stefan Salvatore",
+  ];
+  $( "#tags" ).autocomplete({
+    source: availableTags
   });
-  
-  })
+})
